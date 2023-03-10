0aef15b36baccd6d7df5df8c8843c98b2fd62db7
Akande Al-ameen <longlasting1805@gmail.com>
2023-03-10 17:48:12 +0100

    core app comments

--- core/views.py
+++ core/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import CommentSerializer
+from rest_framework.response import Response 
+from .models import Comment
+
 
 # Create your views here.
+
+class PostCommentAPIView(APIView):
+    def get(self, _, pk=None):
+        comments = Comment.objects.filter(post_id=pk)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    
+class CommentAPIView(APIView):
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)    
+
+
